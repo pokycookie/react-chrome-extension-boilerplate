748b9d2cd65656308dd8ad49e7ae0a2c42fdf87b
pokycookie <0cookieboy0@gmail.com>
2023-09-07 00:25:07 +0900

    init: webpack + react + typescript + tailwind css

---
+++ src/newtab/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const App = () => {
+  return <main className="w-vw h-screen"></main>
+}
+
+export default App
---
+++ src/popup/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const App = () => {
+  return (
+    <div className="w-48 h-32 flex items-center justify-center bg-zinc-200">
+      <p>Hello no CRA</p>
+    </div>
+  )
+}
+
+export default App
